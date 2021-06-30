bbb2c647d42848ca82e779604f4531911c951375
Jonas Frank <jonas@jkfrank.dk>
2021-06-30 10:42:27 +0200

    Initial commit for this folder

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,7 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+"use strict";
 
+const x = 23;
+
+const calcAge = (birthYear) => 2050 - birthYear;
+console.log(calcAge(1970));
